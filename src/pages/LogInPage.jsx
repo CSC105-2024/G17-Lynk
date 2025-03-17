1c2f0ce96c657c649c51234f2ece15a336888940
Bambi <148939281+adorosis@users.noreply.github.com>
2025-03-17 22:56:14 +0700

    wtf

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -3,18 +3,13 @@ import DarkLogo from '../images/LogInDark-img.svg';
 import LightLogo from '../images/LogInLight-img.svg';
 const LogInPage = () => {
   return (
-  <div className='container flex items-center justify-center min-h-screen'>
+  <div className='container flex items-center justify-center min-h-screen bg-black text-white'>
     {/* left Logo and Text */}
-    <div className='left-container w-1/2 flex flex-col items-center text-center justify-center'>
-      <img src={DarkLogo} alt="Dark_Logo" className='w-[500px] h-[500px] mb-4'/>
-      <p>Welcome to LYNk!</p>
-      <p>We List What You Wish</p>
-    </div>
-
-    <div className='right-container'>
-      <p>Hello</p>
-    </div>
-
+    <div className='left-container w-1/2 flex flex-col items-center text-center'>
+      <img src={DarkLogo} alt="LYNk_Logo" className='w-[242px] h-[289px] mb-4'/>
+      <p className='text-3xl font-bold'>Welcome to LYNk!</p>
+      <p className='text-1.5xl'>We List What You Wish</p>
+    </div>  
   </div>
   );
 };
