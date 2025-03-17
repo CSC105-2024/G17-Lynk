4ed9531c2e85cf9280ce6cd6607dc4e1d7e3ea65
Bambi <148939281+adorosis@users.noreply.github.com>
2025-03-17 22:12:36 +0700

    testLogo

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import Dark_Logo from '../images/LogInDark-img.svg'
-import Light_Logo from '../images/LogInLight-img.svg'
+import React from 'react';
+import DarkLogo from '../images/LogInDark-img.svg';
+import LightLogo from '../images/LogInLight-img.svg';
 const LogInPage = () => {
   return (
-    <>
-      "Skibidi Alora"
-    </>
-  )
-}
+  <div className='container flex items-center justify-center min-h-screen'>
+    {/* left Logo and Text */}
+    <div className='left-container w-1/2 flex flex-col items-center text-center justify-center'>
+      <img src={DarkLogo} alt="Dark_Logo" className='w-[500px] h-[500px] mb-4'/>
+      <p>Welcome to LYNk!</p>
+      <p>We List What You Wish</p>
+    </div>
 
-export default LogInPage
+    <div className='right-container'>
+      <p>Hello</p>
+    </div>
+
+  </div>
+  );
+};
+
+export default LogInPage;
