d17fd2c8d15504db332e0dbd67eed0b5df24ed4a
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-03-17 23:44:21 +0700

    Small fix

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import DarkLogo from '../images/LogInDark-img.svg';
+import DarkLogo from '../images/LogoDarkTransparent.svg';
 
 const LogInPage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-black text-white">
       {/* Left Section */}
-      <div className="w-1/2 flex flex-col items-center text-center">
-        <img src={DarkLogo} alt="LYNk Logo" className="w-[242px] h-[289px] mb-4" />
+      <div className="w-1/2 flex flex-col items-center text-center gap-3">
+        <img src={DarkLogo} alt="LYNk Logo" />
         <p className="text-3xl font-bold">Welcome to LYNk!</p>
         <p className="text-xl">We List What You Wish</p>
       </div>
 
       {/* Right Section */}
-      <div className="w-96 p-8 bg-black text-white rounded-lg shadow-lg mx-auto text-center">
+      <div className="w-96 p-8 bg-black text-white rounded-lg shadow-lg mx-auto text-center flex flex-col gap-20">
   <h1 className="text-3xl font-semibold text-center mb-6">Login</h1>
   <form className="space-y-4 flex flex-col gap-3">
     <input
       type="email"
       placeholder="Email"
-      className="w-full h-12 p-3 bg-gray-300 text-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      className="w-full h-12 bg-gray-300 text-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 px-10"
     />
     <input
       type="password"
