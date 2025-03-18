3b7836fc9f737bd616799be6a924294fa72bc990
Bambi <148939281+adorosis@users.noreply.github.com>
2025-03-18 16:04:49 +0700

    ytafjlaksfalsdfjasdfkad

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import LogInPage from "./pages/LogInPage"
 import SignUpPage from "./pages/SignUpPage";
 import LandingPage from './pages/LandingPage';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-    
-      <LandingPage />
+      <LandingPage/>
+      <Outlet/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,23 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LogInPage from './pages/LogInPage.jsx';
 import SignUpPage from './pages/SignUpPage.jsx';
+import LandingPage from './pages/LandingPage.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage/>,
+    children: [
+    
+    ]
   },
   {
     path: "/login",
     element: <LogInPage/>
   },
   {
-    path: '/signin',
-    element: <SignInPage/>
+    path: '/signup',
+    element: <SignUpPage/>
   }
-
 ]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
