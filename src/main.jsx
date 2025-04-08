b904245526d48f294414ca29cafd347efe53cc03
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-04-08 15:21:52 +0700

    Shadcn setup and testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "./styles/index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LogInPage from './pages/LogInPage.jsx';
 import SignUpPage from './pages/SignUpPage.jsx';
 import LandingPage from './pages/LandingPage.jsx';
+import Test from './pages/Test.jsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <LandingPage/>,
-
+    path: '/',
+    element: <LandingPage />,
   },
   {
-    path: "/login",
-    element: <LogInPage/>
+    path: '/login',
+    element: <LogInPage />,
   },
   {
     path: '/signup',
-    element: <SignUpPage/>
-  }
+    element: <SignUpPage />,
+  },
+  {
+    path: '/test',
+    element: <Test />,
+  },
 ]);
 createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} /> 
-)
+  <RouterProvider router={router} />
+);
