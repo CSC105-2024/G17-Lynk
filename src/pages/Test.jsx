81f9b0de508c63e7c68ff7e33d8bfedf6e075507
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-04-08 15:21:52 +0700

    Shadcn setup and testing

---
+++ src/pages/Test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+
+const Test = () => {
+  return <Button variant='destructive'>Destructive</Button>;
+};
+
+export default Test;
