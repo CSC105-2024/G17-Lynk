384ebd193b91841e86f6cfd90fe463efc2f1f21a
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-04-09 20:15:48 +0700

    Dashboard finish

--- src/components/App/InfoCard.jsx
+++ src/components/App/InfoCard.jsx
@@ -4,30 +4,30 @@ import { Separator } from '@/components/ui/separator';
 
 const InfoCard = () => {
   return (
-    <div className='grid grid-cols-3 bg-[var(--info-card-bg-color)] rounded-2xl m-5 p-4'>
-      <div className=' border-r-2 flex p-5 justify-center items-center'>
+    <div className='grid md:grid-cols-3 bg-[var(--info-card-bg-color)] rounded-2xl py-5 '>
+      <div className=' md:border-r-2 flex p-5 justify-center items-center'>
         <div className='p-2.5 text-2xl bg-[var(--info-icon-bg-color)] rounded-lg mr-5 text-blue-500'>
           <IoMdLink />
         </div>
-        <div>
+        <div className='w-3'>
           <p className='text-3xl'>3</p>
           <p>Links</p>
         </div>
       </div>
-      <div className=' border-r-2 flex p-5 justify-center items-center'>
+      <div className=' md:border-r-2 flex p-5 justify-center items-center'>
         <div className='p-2.5 text-2xl bg-[var(--info-icon-bg-color)] rounded-lg mr-5 text-blue-500'>
           <IoMdLink />
         </div>
-        <div>
+        <div className='w-3'>
           <p className='text-3xl'>3</p>
           <p>Playlists</p>
         </div>
       </div>
-      <div className='flex p-5 justify-center items-center'>
+      <div className=' flex p-5 justify-center items-center'>
         <div className='p-2.5 text-2xl bg-[var(--info-icon-bg-color)] rounded-lg mr-5 text-blue-500'>
           <IoMdLink />
         </div>
-        <div>
+        <div className='w-3'>
           <p className='text-3xl'>3</p>
           <p>Tags</p>
         </div>
