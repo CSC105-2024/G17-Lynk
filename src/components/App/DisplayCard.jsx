3f3624dbad2a4a600120a1ec73e46b0ca10acd9b
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-04-09 19:03:18 +0700

    Style

--- src/components/App/DisplayCard.jsx
+++ src/components/App/DisplayCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DisplayCard = ({ icon, title, subTitle, children }) => {
   return (
     <>
-      <div className='flex bg-red-500'>
+      <div className='flex'>
         <div className='text-2xl p-2'>{icon}</div>
         <div>
           <h2 className='text-xl font-bold'>{title}</h2>
--- src/components/App/SideBarMenuLink.jsx
+++ src/components/App/SideBarMenuLink.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SideBarMenuLink = ({ icon, name, number }) => {
   return (
-    <div className='flex items-center bg-blue-500 my-2 py-1 px-4'>
+    <div className='flex items-center hover:bg-[var(--hover-sidemenu-bg)] my-2 py-1 px-4 rounded-lg cursor-pointer'>
       <div>{icon}</div>
       <p className='flex-grow ml-2'>{name}</p>
       <p>{number}</p>
