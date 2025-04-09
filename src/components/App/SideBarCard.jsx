79b429fc53a8fcc66a738f0ea402c443022f803a
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-04-09 18:57:03 +0700

    Layout

--- src/components/App/SideBarCard.jsx
+++ src/components/App/SideBarCard.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import SideBarMenuLink from './SideBarMenuLink';
+import { MdHome, MdTimelapse } from 'react-icons/md';
+import { Separator } from '@/components/ui/separator';
 
 const SideBarCard = () => {
   return (
-    <div className='bg-amber-400 min-h-screen'>
-      <div></div>
+    <div className='bg-amber-400 min-w-60 hidden sm:block py-2 px-5'>
+      <div className='w-20'>
+        <img src='/logo-dark-v2.svg' alt='Logo' />
+      </div>
+      <div className='m-3'>
+        <SideBarMenuLink icon={<MdHome />} name='Home' number='3' />
+        <SideBarMenuLink icon={<MdHome />} name='Home' number='3' />
+        <SideBarMenuLink icon={<MdHome />} name='Home' number='3' />
+      </div>
+      <Separator />
+      <div className='m-3'>
+        <h2 className='text-lg'>Playlist</h2>
+        <SideBarMenuLink icon={<MdHome />} name='Home' number='3' />
+        <SideBarMenuLink icon={<MdHome />} name='Home' number='3' />
+        <SideBarMenuLink icon={<MdHome />} name='Home' number='3' />
+      </div>
+      <Separator />
+      <div className='text-xs m-3'>
+        <h2 className='text-lg'>Tags</h2>
+        <p>#Tag1</p>
+        <p>#Tag1</p>
+        <p>#Tag1</p>
+      </div>
     </div>
   );
 };
