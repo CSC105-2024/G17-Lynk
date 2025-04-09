56e2693ee639fb2c3eb4f53927815e2b1672743e
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-04-09 15:43:32 +0700

    Init dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import AppSidebar from '@/components/AppSidebar';
+
 const App = () => {
   return (
     <>
-     <LandingPage/> 
-      <Outlet/>
+      {/* <LandingPage /> */}
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+          <Outlet />
+        </main>
+      </SidebarProvider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
