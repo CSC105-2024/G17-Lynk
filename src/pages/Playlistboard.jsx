102968ca681b5668b02bb526560b410a7012d8f9
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-04-12 19:30:45 +0700

    Updated Icon and Name display in Playlistboard

--- src/pages/Playlistboard.jsx
+++ src/pages/Playlistboard.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { dummyData } from '@/services/data';
+import { dummyData, dummyPlaylist } from '@/services/data';
 import LinkCard from '@/components/App/LinkCard';
+import APP_ICONS from '@/constants/icons.js';
 
 const Playlistboard = () => {
-  const data = dummyData;
+  const bookmarkData = dummyData;
+  const playlistData = dummyPlaylist;
   const { playlistId } = useParams();
+  const iconLink = playlistData[playlistId].iconLink;
+  const IconComponent = APP_ICONS[iconLink];
   return (
     <div>
-      <div>icon</div>
-      <h2>name</h2>
-      {data.map((item, idx) => (
+      <div>{<IconComponent />}</div>
+      <h2>{playlistData[playlistId].name}</h2>
+      {bookmarkData.map((item, idx) => (
         <LinkCard key={idx} data={item} />
       ))}
       {playlistId}
