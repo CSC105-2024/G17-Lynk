6d8767e9884ec6184ebdea929237769991272e08
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-04-15 10:13:22 +0700

    Updated css for active sidebar menu

--- src/components/App/SideBarMenuLink.jsx
+++ src/components/App/SideBarMenuLink.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const SideBarMenuLink = ({ icon, name, number, link }) => {
   return (
-    <Link
+    <NavLink
       to={link}
-      className='flex items-center hover:bg-[var(--hover-sidemenu-bg)] my-2 py-1 px-4 rounded-lg cursor-pointer'
+      // className='flex items-center hover:bg-[var(--hover-sidemenu-bg)] my-2 py-1 px-4 rounded-lg cursor-pointer'
+      className={({ isActive }) =>
+        `flex items-center hover:bg-[var(--hover-sidemenu-bg)] my-2 py-1 px-4 rounded-lg cursor-pointer ${
+          isActive
+            ? 'bg-[var(--active-sidemenu-bg)] text-[var(--active-sidemenu-text)]'
+            : ''
+        }`
+      }
     >
       <div>{icon}</div>
       <p className='flex-grow ml-2'>{name}</p>
       <p>{number}</p>
-    </Link>
+    </NavLink>
   );
 };
 
