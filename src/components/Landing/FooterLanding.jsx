90ab3a81c8cccfd8d306de0ec1d57ded54b6823b
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-04-15 14:39:48 +0700

    Fix customize style

--- src/components/Landing/FooterLanding.jsx
+++ src/components/Landing/FooterLanding.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => (
   <div
-    className='flex flex-col sm:flex-row items-center justify-between font-sans py-8 gap-4 px-10 md:px-15 lg:px-20 xl:px-40'
-    style={{ backgroundColor: 'var(--sec-dark-color)' }}
+    className='flex flex-row items-center justify-between font-sans py-8 gap-4 px-10 md:px-15 lg:px-20 xl:px-40'
+    style={{ backgroundColor: 'var(--footer-bg-color)' }}
   >
-    <img src='./logo-dark-v1.svg' alt='lynk Logo' className='w-28 sm:w-36' />
+    <img src='./logo-dark-v1.svg' alt='lynk Logo' className='w-15' />
     <div className='text-sm text-gray-400 font-medium text-center sm:text-left'>
       ©2025 lynk.com
     </div>
