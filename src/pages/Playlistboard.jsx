06b37b0019a24fad53268ec63835895de5e12418
Nang Hayman Aye Mya <nanghaymanayemya@gmail.com>
2025-04-17 15:07:10 +0700

    Fix icons, titles, descriptions for bookmark collection display pages

--- src/pages/Playlistboard.jsx
+++ src/pages/Playlistboard.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { dummyData, dummyPlaylist } from '@/services/data';
 import LinkCard from '@/components/App/LinkCard';
 import APP_ICONS from '@/constants/icons.js';
+import DisplayCard from '@/components/App/DisplayCard';
+import { MdHome } from 'react-icons/md';
 
 const Playlistboard = () => {
   const [bookmarkData, setBookmarkData] = useState(dummyData);
@@ -18,13 +20,16 @@ const Playlistboard = () => {
   };
   return (
     <div>
-      <div>{IconComponent ? <IconComponent /> : null}</div>
-      <h2>{playlistData[playlistId - 1].name}</h2>
-      {bookmarkData.map((item, idx) => (
-        <LinkCard key={idx} data={item} onDelete={handleDelete} />
-      ))}
-      {playlistId}
+      Testing playlist id: {playlistId}
       {console.log('render here')}
+      <DisplayCard
+        icon={IconComponent ? <IconComponent /> : null}
+        title={playlistData[playlistId - 1].name}
+        subTitle={playlistData[playlistId - 1].description}
+        children={bookmarkData.map((item, idx) => (
+          <LinkCard key={idx} data={item} onDelete={handleDelete} />
+        ))}
+      ></DisplayCard>
     </div>
   );
 };
