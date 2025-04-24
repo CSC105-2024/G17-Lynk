811c41e68e6002f629091eb51f87ae4cebabf530
TakaXTakaX <swanyeeht@gmail.com>
2025-04-24 14:58:21 +0700

    landing done

--- src/components/Landing/FeatureLanding.jsx
+++ src/components/Landing/FeatureLanding.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { paragraph, title as titleStyle } from '../../styles/styles';
+import { paragraph, title as titleStyle } from '../../styles/styles';  // Importing custom styles for paragraph and title
 
 const FeatureLanding = ({
   title,
@@ -13,6 +13,7 @@ const FeatureLanding = ({
       'py-8 sm:py-12 lg:py-20 bg-[var(--feature-bg-color)] px-10 md:px-15 lg:px-20 xl:px-40'
     }
   >
+    {/* Wrapper div with conditional layout based on the 'reverse' prop */}
     <div
       className={`flex flex-col sm:flex-row sm:justify-between sm:items-center gap-10 ${
         reverse ? 'sm:flex-row-reverse' : 'sm:flex-row'
@@ -24,17 +25,20 @@ const FeatureLanding = ({
           reverse ? 'sm:text-right' : 'sm:text-left'
         } flex flex-col gap-5 md:gap-12`}
       >
+        {/* Title */}
         <h2
           className={`${titleStyle} text-[var(--text-primary-color)]`}
-          style={{ color: headerStyle }}
+          style={{ color: headerStyle }}  // Apply custom header color
         >
           {title}
         </h2>
+        {/* Description */}
         <p className={`${paragraph}`}>{description}</p>
       </div>
 
       {/* Image Section */}
       <div className='w-full md:w-md xl:w-2xl h-auto flex justify-center sm:justify-end'>
+        {/* Image with responsive size and rounded corners */}
         <img
           src={image}
           alt={title}
@@ -45,4 +49,4 @@ const FeatureLanding = ({
   </section>
 );
 
-export default FeatureLanding;
+export default FeatureLanding;  // Export FeatureLanding component for use in other parts of the application
