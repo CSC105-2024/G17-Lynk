a8235e80f7f47ac571568e00c768d009fe86660e
TakaXTakaX <swanyeeht@gmail.com>
2025-05-06 03:22:18 +0700

    playlists post and get

--- Backend/src/routes/playlist.route.ts
+++ Backend/src/routes/playlist.route.ts
@@ -5,5 +5,6 @@ const playlistRouter = new Hono();
 
 playlistRouter.post("/", playlistController.createPlaylist);
 playlistRouter.get("/user/:userId", playlistController.getUserPlaylists);
+playlistRouter.get("/:playlistId/links", playlistController.getLinksInPlaylist);
 
 export { playlistRouter };
