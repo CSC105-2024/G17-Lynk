f651ae8722cbcd536a35b7c4bdd1e695609d32d7
TakaXTakaX <swanyeeht@gmail.com>
2025-05-17 12:59:37 +0700

    added link edit delete and create delete tags

--- Backend/src/routes/link.route.ts
+++ Backend/src/routes/link.route.ts
@@ -8,5 +8,6 @@ linkRouter.get("/user/:userId", linkController.getLinksByUser);
 linkRouter.post("/add-to-playlist", linkController.addLinkToPlaylist);
 linkRouter.put("/:linkId", linkController.editLink);
 linkRouter.delete("/:linkId", linkController.deleteLink);
+//taka added these
 
 export { linkRouter };
